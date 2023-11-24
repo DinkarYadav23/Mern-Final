18c538a74da9335a99c31007bfb789eb405af50f
unknown <DinkarYadav23>
2023-11-25 02:09:54 +0530

    project init
    Signed-off-by: unknown <DinkarYadav23>

---
+++ frontend/src/hooks/useLogin.js
@@ -0,0 +1,34 @@
+// useLogin.js
+
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+
+export function useLogin() {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const login = async (username, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch(`${process.env.REACT_APP_URL}/api/user/login`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const result = await response.json();
+
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(result.error);
+    } else {
+      localStorage.setItem('user', JSON.stringify(result));
+      dispatch({ type: 'LOGIN', payload: result });
+      setIsLoading(false);
+    }
+  };
+
+  return { login, isLoading, error };
+}
---
+++ frontend/src/hooks/useSignup.js
@@ -0,0 +1,33 @@
+// useSignup.js
+
+import { useState } from 'react';
+import { useAuthContext } from './useAuthContext';
+
+export function useSignup() {
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const { dispatch } = useAuthContext();
+
+  const signup = async (username, password) => {
+    setIsLoading(true);
+    setError(null);
+
+    const response = await fetch(`${process.env.REACT_APP_URL}/api/user/signup`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password }),
+    });
+
+    const result = await response.json();
+    if (!response.ok) {
+      setIsLoading(false);
+      setError(result.error);
+    } else {
+      localStorage.setItem('user', JSON.stringify(result));
+      dispatch({ type: 'LOGIN', payload: result });
+      setIsLoading(false);
+    }
+  };
+
+  return { signup, isLoading, error };
+}
